ac25ca41b6b5f48167d30b4dc3cf3ad14416792a
Cash10 <ashwin2crazy@gmail.com>
2015-06-04 23:23:58 +0530

    Delete script2.py

--- script2/script2.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/python
-import MySQLdb
-db=MySQLdb.connect(host="localhost",user="root",passwd="********")
-cursor=db.cursor()
-print "inserting values"
-db = MySQLdb.connect("localhost","root","*******","DO" )
-cursor = db.cursor()
-sql = """insert into student (time) values(now())"""
-cursor.execute(sql)
