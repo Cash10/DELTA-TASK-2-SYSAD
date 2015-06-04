7ea38aa85daf4c5bcc2f5516a333e4bdfdfcd5ce
Cash10 <ashwin2crazy@gmail.com>
2015-06-04 23:03:05 +0530

    Update script1.py

--- script1/script1.py
+++ script1/script1.py
@@ -1,6 +1,7 @@
-#! /usr/bin/python
+#! /usr/bin/env python
 import MySQLdb
-db=MySQLdb.connect(host="localhost",user="root",passwd="********")
+file1=open("path/script1.py","a+")
+file1.write("db=MySQLdb.connect(host="localhost",user="root",passwd="********")
 cursor=db.cursor()
 sql='CREATE DATABASE DO'
 print "CREATE DATABASE"
@@ -8,5 +9,16 @@ cursor.execute(sql)
 db = MySQLdb.connect("localhost","root","*******","DO" )
 cursor = db.cursor()
 sql = """CREATE TABLE student (time DATETIME )"""
-cursor.execute(sql)
+cursor.execute(sql) ")
+file1.close()
+file2=open("path/script2.py,"a+")
+file2.write("db=MySQLdb.connect(host="localhost",user="root",passwd="********")
+cursor=db.cursor()
+print "inserting values"
+db = MySQLdb.connect("localhost","root","*******","DO" )
+cursor = db.cursor()
+sql = """insert into student (time) values(now())"""
+cursor.execute(sql) ")
+file2.close()
+
 
